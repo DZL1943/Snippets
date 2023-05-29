243bd28d93bf251deff72f1bb3eb97078a0878b9
DZL1943 <ldz1943@163.com>
2023-05-29 20:48:58 +0800

    1

---
+++ python/guessNumber.py
@@ -0,0 +1,24 @@
+def guessNumber(times=10):
+    print(
+f"""猜数字游戏
+- 随机生成一个 [1, 100] 的数
+- 共有 {times} 次猜测机会
+- 猜错提示大小
+""")
+    import random
+    number = random.randint(1, 100)
+
+    while times > 0:
+        n = int(input(f"猜测一个 [1, 100] 的整数, 还有 {times} 次机会: "))
+        if n == number:
+            print("猜对了!")
+            break
+        else:
+            times -= 1
+            print("大了" if n > number else "小了")
+    else:
+        print(f"Game over... the answer is {number}")
+
+
+if __name__ == "__main__":
+    guessNumber()
